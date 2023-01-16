c3ec53978b39264cc849d27ea916e8d325707b5b
vivianne <viviannjoroge91@gmail.com>
2023-01-16 05:27:52 +0300

    Completed assignment

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function NewTaskForm() {
+function NewTaskForm({onTaskFormSUbmit,categories}) {
   return (
     <form className="new-task-form">
       <label>
@@ -11,6 +11,10 @@ function NewTaskForm() {
         Category
         <select name="category">
           {/* render <option> elements for each category here */}
+          {categories.map((category,index)=>(
+            <option key={index}>{category}</option>
+          ))}
+
         </select>
       </label>
       <input type="submit" value="Add task" />
